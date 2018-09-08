9139ad5f100ac6473506ee35de8fd02976ac8f5f
alekkxl <35567115+alekkxl@users.noreply.github.com>
2018-09-08 20:23:45 +0800

    更新

---
+++ transmission.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt install -y transmission-daemon transmission-remote-cli wget
+service transmission-daemon stop
+cp settings.json /var/lib/transmission-daemon/info/settings.json
+service transmission-daemon start
+sed '2 i service transmission-daemon start' -i /etc/rc.local
+mkdir -p /usr/local/caddy/www
+chmod -R 777 /usr/local/caddy/www
