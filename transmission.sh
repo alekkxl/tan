d2c4b6ba1231860b21c0b169cefb59f7478ef40a
alekkxl <35567115+alekkxl@users.noreply.github.com>
2018-09-08 20:46:35 +0800

    更新

--- transmission.sh
+++ transmission.sh
@@ -6,5 +6,5 @@ service transmission-daemon stop
 cp settings.json /var/lib/transmission-daemon/info/settings.json
 service transmission-daemon start
 sed '2 i service transmission-daemon start' -i /etc/rc.local
-mkdir -p /usr/local/caddy/www
+mkdir -p /usr/local/caddy/www/Download
 chmod -R 777 /usr/local/caddy/www
